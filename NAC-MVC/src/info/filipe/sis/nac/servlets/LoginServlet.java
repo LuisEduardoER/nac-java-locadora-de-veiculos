68bcad4045c9c574f95759e734c81ccb7d254017
epilif09 <epilif09@gmail.com>
2014-05-14 23:15:36 +0000

    Sistema de login sem MD5 funcional.

---
+++ NAC-MVC/src/info/filipe/sis/nac/servlets/LoginServlet.java
@@ -0,0 +1,57 @@
+package info.filipe.sis.nac.servlets;
+
+import info.filipe.sis.nac.bean.Login;
+import info.filipe.sis.nac.dao.LoginDAO;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	LoginDAO dao = new LoginDAO();
+	
+	public LoginServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		//not available
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		if(!request.getParameter("login").equals("") && !request.getParameter("passwd").equals("")){
+			Login l = new Login();
+			l.setUsuario(request.getParameter("login"));
+			l.setSenha(request.getParameter("passwd"));
+			
+			if(dao.verifyLogin(l)){
+				HttpSession session = request.getSession(true);
+				session.setAttribute("logininfo", l);
+				dispatcher(request, response);
+			} else {
+				System.out.println("Falhou.");
+			}
+			
+		}
+	}
+	
+	private void dispatcher(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String pagina = "index.html";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(pagina);
+		dispatcher.forward(request, response);
+	}
+
+}
