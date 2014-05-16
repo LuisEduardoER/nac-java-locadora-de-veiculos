514b823e932d1175bf7b75a4c8d9037412348051
epilif09@gmail.com <epilif09@gmail.com@12ee23f8-2c0e-1349-560a-340498245636>
2014-05-16 15:29:54 +0000

    Inclusão da Logon.class para uso do isLogado nos servlets.

--- NAC-MVC/src/info/filipe/sis/nac/servlets/Carros.java
+++ NAC-MVC/src/info/filipe/sis/nac/servlets/Carros.java
@@ -2,6 +2,7 @@ package info.filipe.sis.nac.servlets;
 
 import info.filipe.sis.nac.bean.Carro;
 import info.filipe.sis.nac.dao.CarroDAO;
+import info.filipe.sis.nac.login.Logon;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,17 +18,26 @@ import javax.servlet.http.HttpServletResponse;
 public class Carros extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     CarroDAO dao = new CarroDAO();
+    Logon logon = new Logon();
     
     public Carros() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		listar(request, response);
+		if(logon.isLogado(request, response)){
+			listar(request, response);			
+		} else {
+			response.sendRedirect("login.jsp");
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		listar(request, response);
+		if(logon.isLogado(request, response)){
+			listar(request, response);			
+		} else {
+			response.sendRedirect("login.jsp");
+		}
 	}
 
 	private void listar(HttpServletRequest request, HttpServletResponse response)
